fb4ac441d76d933a7eb87af2e42f1cd774a425ce
Mario Acedo Caballero <mario.acedo@atmira.com>
2022-07-13 12:34:17 +0200

    Instalación del swagger y configuración del mismo en el startup

--- NasaAPI/Startup.cs
+++ NasaAPI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,15 @@ namespace NasaAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "1.0",
+                    Title = "Ejemplo api net core curso Udemy"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +45,13 @@ namespace NasaAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(cfg =>
+            {
+                cfg.SwaggerEndpoint("/swagger/v1/swagger.json", "Ejemplo api net core curso udemy");
+            });
 
             app.UseHttpsRedirection();
 

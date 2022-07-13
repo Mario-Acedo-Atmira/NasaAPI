895a274052ffdfca52fd213193a3399a0ce6a160
Mario Acedo Caballero <mario.acedo@atmira.com>
2022-07-13 12:53:18 +0200

    Configuración final del model controller con el IAPI y el metodo GET que devolvera el top 3 de meteoritos y se ha cambiado la configuración de al abrir el explorador que salga el swagger directamente

--- NasaAPI/Controllers/ModelController.cs
+++ NasaAPI/Controllers/ModelController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NasaAPI.DataAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,34 @@ namespace NasaAPI.Controllers
     [ApiController]
     public class ModelController : ControllerBase
     {
+        private readonly IAPI _model;
+
+        public ModelController(IAPI model)
+        {
+            _model = model;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetWhere(int days)
+        {
+
+            if (days > 7 || days <= 0)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                var entity = _model.Obtenertop3(days);
+                if (entity.Result.Count == 0)
+                {
+                    return Ok(entity);
+                }
+
+                return Ok(entity);
+            }
+        }
 
     }
+
+    
 }

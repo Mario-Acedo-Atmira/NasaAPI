46fbec937d2a20c7f34984d105eed05d414b4821
Mario Acedo Caballero <mario.acedo@atmira.com>
2022-07-15 12:06:09 +0200

    Moq

--- NasaAPI.UnitTest/UnitTest1.cs
+++ NasaAPI.UnitTest/UnitTest1.cs
@@ -1,8 +1,11 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using NasaAPI.Controllers;
 using NasaAPI.DataAccess;
+using NasaAPI.Models;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -10,49 +13,46 @@ namespace NasaAPI.UnitTest
 {
     public class UnitTest1
     {
+        private readonly ModelController _model;
+        private readonly Mock<API> _apimock=new Mock<API>();
+        
+        public UnitTest1()
+        {
+            _model = new ModelController(_apimock.Object);
+        }
+        
+        
         [Fact]
         public async Task CasoValido()
         {
 
-            //Arrange
-            var model = new API();
-            var controller = new ModelController(model);
+            int dia = 4;
+            var customer = (OkObjectResult)await _model.GetTop3(dia);
+            var ok= (OkObjectResult)await _model.GetTop3(dia);
 
-            //Act
-            var actionResult = (OkObjectResult)await controller.GetTop3(6);
-
-            //Assert
-            actionResult.StatusCode.Should().Be(200);
+            Assert.Equal(customer.ToString(), ok.ToString());
         }
 
         [Fact]
         public async Task CasoNoValido()
         {
 
-            //Arrange
-            var model = new API();
-            var controller = new ModelController(model);
+            int dia = 8;
+            var customer = (BadRequestResult)await _model.GetTop3(dia);
+            var bad = (BadRequestResult)await _model.GetTop3(dia);
 
-            //Act
-            var actionResult = (BadRequestResult)await controller.GetTop3(8);
-
-            //Assert
-            actionResult.StatusCode.Should().Be(400);
+            Assert.Equal(customer.ToString(), bad.ToString());
         }
 
         [Fact]
         public async Task CasoVacio()
         {
 
-            //Arrange
-            var model = new API();
-            var controller = new ModelController(model);
+            int dia = 1;
+            var customer = (OkObjectResult)await _model.GetTop3(dia);
+            var ok = (OkObjectResult)await _model.GetTop3(dia);
 
-            //Act
-            var actionResult = (OkObjectResult)await controller.GetTop3(1);
-
-            //Assert
-            actionResult.StatusCode.Should().Be(200);
+            Assert.Equal(customer.ToString(), ok.ToString());
         }
     }
 }
